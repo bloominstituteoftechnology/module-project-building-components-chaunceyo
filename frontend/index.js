1834690ada4657f90bbe708de39d3f0689548a1a
Chaunceyo <chaunceyogletree725@gmail.com>
2024-03-27 19:34:33 -0400

    finished

--- frontend/index.js
+++ frontend/index.js
@@ -3,8 +3,15 @@ function moduleProject3() {
   // 👉 TASK 1 - Write a `buildNav` component that returns a nav
 
   function buildNav(links) {
-    //  ✨ do your magic here
-    return document.createElement('nav')
+    let returnNav = document.createElement('nav')
+   links.forEach((link) => {
+    const thisLink = document.createElement('a');
+    thisLink.href = link.href
+    thisLink.textContent = link.textContent
+    thisLink.title = link.title
+    returnNav.appendChild(thisLink)
+   })
+    return returnNav;
   }
 
   // ❗ DOM creation using your `buildNav` component (do not change):
@@ -20,6 +27,37 @@ function moduleProject3() {
 
   function buildLearnerCard(learner, languages) {
     //  ✨ do your magic here
+    const card = document.createElement('div')
+    card.classList.add('learner-card');
+    const cardName = document.createElement('p')
+    cardName.textContent = learner.fullName
+    const cardID = document.createElement('p')
+    cardID.textContent = `Learner ID: ${learner.id}`
+    const cardDOB = document.createElement('p')
+    cardDOB.textContent = `Date of Birth: ${learner.dateOfBirth}`
+    const favoriteLanguage = document.createElement('p')
+    let thisLanguage;
+    languages.forEach((language) => {
+      if(language.id === learner.favLanguage){
+        thisLanguage = language.name
+      }
+    })
+    favoriteLanguage.textContent = `Favorite Language: ${thisLanguage}`
+    card.appendChild(cardName)
+    card.appendChild(cardID)
+    card.appendChild(cardDOB)
+    card.appendChild(favoriteLanguage)
+    card.addEventListener('click',() => {
+      let allCards = document.querySelectorAll('.learner-card')
+      allCards.forEach((thisCard) => {
+        if(thisCard === card){
+          card.classList.add('active')
+        }else if(thisCard.classList.contains('active')){
+          thisCard.classList.remove('active')
+        }
+      })
+    })
+    return card
   }
 
   {
@@ -40,13 +78,56 @@ function moduleProject3() {
       { id: 17, fullName: 'Daniel Castillo', dateOfBirth: '1995-11-05', favLanguage: 12 }
     ]
     //  ✨ do your magic here
+    learners.forEach((learner) => {
+     const learnerCard  = buildLearnerCard(learner,languages)
+      document.querySelector('section').appendChild(learnerCard)
+      
+    })
+   
+    
   }
-
   // 👉 TASK 3 - Write a `buildFooter` component that returns a footer
 
   function buildFooter(footerData) {
     //  ✨ do your magic here
-    return document.createElement('footer')
+    const footer =  document.createElement('footer')
+    const companyInfo = document.createElement('div')
+    const companyName =  document.createElement('p')
+    companyName.classList.add('company-name')
+    companyName.textContent = footerData.companyName
+    const address =  document.createElement('p')
+    address.classList.add('address')
+    address.textContent = footerData.address
+    let contactEmail =  document.createElement('p')
+    contactEmail.classList.add('contact-email')
+    contactEmail.textContent = "Email: "
+    let  emailLink = document.createElement('a')
+    emailLink.setAttribute('href' , 'mailto:info@example.com')
+    emailLink.text = 'info@example.com'
+    contactEmail.appendChild(emailLink)
+    const socialMedia =  document.createElement('div')
+    socialMedia.classList.add('social-media')
+    const twitterLink =  document.createElement('a')
+    twitterLink.textContent = 'Twitter'
+    twitterLink.setAttribute('href' , 'https://twitter.com/example')
+    const facebookLink =  document.createElement('a')
+    facebookLink.textContent = 'Facebook'
+    facebookLink.setAttribute('href' , 'https://www.facebook.com/example')
+    const instaLink = document.createElement('a')
+    instaLink.setAttribute('href' , 'https://www.instagram.com/example')
+    instaLink.textContent = 'Instagram'
+    const bloom =  document.createElement('div')
+    bloom.textContent = '© BLOOM INSTITUTE OF TECHNOLOGY 2024'
+    footer.appendChild(companyInfo)
+    companyInfo.appendChild(companyName)
+    companyInfo.appendChild(address)
+    companyInfo.appendChild(contactEmail)
+    footer.appendChild(socialMedia)
+    socialMedia.appendChild(twitterLink)
+    socialMedia.appendChild(facebookLink)
+    socialMedia.appendChild(instaLink)
+    footer.appendChild(bloom)
+    return footer
   }
 
   // ❗ DOM creation using your `buildFooter` component (do not change):
@@ -62,9 +143,15 @@ function moduleProject3() {
   }))
 
   // 👉 TASK 4 - Clicking on the section should deactivate the active card
-
-  //  ✨ do your magic here
-}
+ //  ✨ do your magic here
+ const section = document.querySelector('section')
+  section.addEventListener('click', (evt) => {
+    const activeCard = document.querySelector('.active')
+    if (evt.target === section && section.contains(activeCard)){ 
+    activeCard.classList.remove('active')
+    }
+  })
+ }
 
 // ❗ DO NOT CHANGE THIS CODE
 // ❗ DO NOT CHANGE THIS CODE
